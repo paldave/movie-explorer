d5569227833c8b8228ff20610dd6cabb09f41412
Dawid Paliga <dawid.paliga@7bulls.com>
2019-08-04 10:38:34 +0100

    Removed unused files

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,6 @@
 import { combineReducers } from 'redux';
-import userInterface from './userInterface';
 import moviesNowPlaying from './movies/nowPlaying';
 
 export default combineReducers({
-  userInterface,
   moviesNowPlaying
 });
