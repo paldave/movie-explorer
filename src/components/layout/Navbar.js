9ddbaa64ac31b90dcd39ab2a5b477ca0fe8b1307
Dawid Paliga <dawid.paliga@7bulls.com>
2019-08-29 21:23:19 +0100

    New fonts; Basic material styleguide colours

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import logo from '../../images/tmdbp_logo.png';
+import logo from '../../images/astronaut.svg';
 import './Navbar.scss';
 
 export default class Navbar extends Component {
@@ -7,13 +7,14 @@ export default class Navbar extends Component {
     return (
       <header className="navbar" id="main-nav">
         <div className="navbar-left">
-          <a className="navbar-brands">
-            <img className="navbar-logo-api" src={logo} alt="Powered by TMDB" />
+          <a className="navbar-brands slide-left">
+            <img className="navbar-logo" src={logo} alt="Astronaut" />
+            <div className="navbar-name">TMEX</div>
           </a>
         </div>
         <div className="navbar-right">
           <ul> 
-            <li>TODO_B</li>
+            <li>TODO_A</li>
             <li>TODO_B</li>
           </ul>          
         </div>
