a3381a6e6cfbe6ed2f327af1198ba8ceae0ffafd
Dawid Paliga <dawid.paliga@7bulls.com>
2019-09-06 21:12:50 +0100

    Improved splash screen - new loading animation;

--- src/components/layout/SplashScreen.js
+++ src/components/layout/SplashScreen.js
@@ -1,9 +1,24 @@
 import React, { Component } from 'react'
+import './SplashScreen.scss';
 
 export default class Navbar extends Component {
   render() {
     return (
-      <h1>Loading</h1>
+      <div className="splash-screen">
+        <div className="loader-solar">
+          <div className="orbit orbit-first">
+            <div className="planet" />
+            <div className="orbit orbit-second">
+              <div className="planet" />
+              <div className="orbit orbit-third">
+                <div className="planet" />
+                <div className="planet-main" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <p>Loading...</p>
+      </div>
     )
   }
 }
