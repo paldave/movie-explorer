e1e8c4e6e480999011f6454994f414d28faf55b8
Dawid Paliga <dawid.paliga@7bulls.com>
2020-01-02 22:37:57 +0000

    Renamed SplashScreen class

--- src/components/layout/SplashScreen.js
+++ src/components/layout/SplashScreen.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import './SplashScreen.scss';
 
-export default class Navbar extends Component {
+export default class SplashScreen extends Component {
   render() {
     return (
       <div className="splash-screen">
