7ed3a396d27ef6e44e086406b431ac6b19edda5e
Dawid Paliga <paligadaw@gmail.com>
2020-04-30 23:49:32 +0100

    Updates

---
+++ precache-manifest.62e0aee0c006c12739db52e1bf85b1a0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae06e5900244e877741c6f7e71ede297",
+    "url": "/movie-explorer/index.html"
+  },
+  {
+    "revision": "a7a409ef3ba15a459f9d",
+    "url": "/movie-explorer/static/css/2.b90ce945.chunk.css"
+  },
+  {
+    "revision": "af0a40428f59ddd3ed55",
+    "url": "/movie-explorer/static/css/main.e332bd74.chunk.css"
+  },
+  {
+    "revision": "a7a409ef3ba15a459f9d",
+    "url": "/movie-explorer/static/js/2.70399700.chunk.js"
+  },
+  {
+    "revision": "a4a4865618ae8d63f70e166c2b97e001",
+    "url": "/movie-explorer/static/js/2.70399700.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af0a40428f59ddd3ed55",
+    "url": "/movie-explorer/static/js/main.af0f0e7e.chunk.js"
+  },
+  {
+    "revision": "505494d1fd4af5c57804",
+    "url": "/movie-explorer/static/js/runtime-main.13e1181c.js"
+  },
+  {
+    "revision": "4440e9a919407a65b8ad6a714a7d9aef",
+    "url": "/movie-explorer/static/media/astronaut.4440e9a9.svg"
+  }
+]);
